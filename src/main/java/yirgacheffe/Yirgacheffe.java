769c4edd91137d78014c5d897326f17523cf85cf
MartinRixham <martin.rixham@gmail.com>
2016-12-11 10:41:40 +0000

    Build script.

---
+++ src/main/java/yirgacheffe/Yirgacheffe.java
@@ -0,0 +1,13 @@
+package yirgacheffe;
+
+public final class Yirgacheffe
+{
+	private Yirgacheffe()
+	{
+	}
+
+	public static void main(String[] args)
+	{
+		System.out.println("I did a thing!");
+	}
+}
