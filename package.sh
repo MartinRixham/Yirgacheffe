c1861bd17e0877dfa6c6bd9fc27b35919f3dcb26
MartinRixham <martin.rixham@gmail.com>
2017-12-08 00:14:08 +0530

    Build creates executable.

---
+++ package.sh
@@ -0,0 +1,4 @@
+#! /usr/bin/env bash
+
+cat run.sh target/yirgacheffe-jar-with-dependencies.jar > target/yirgacheffe
+chmod +x target/yirgacheffe
--- run.sh
+++ run.sh
@@ -5,4 +5,7 @@ SOURCE_CODE=$(cat $SOURCE_FILE)
 CLASS_NAME=$(echo $SOURCE_FILE | cut -f 1 -d '.')
 OUTPUT_FILE=$CLASS_NAME".class"
 
-java -jar target/Yirgacheffe.jar "$SOURCE_CODE" > $OUTPUT_FILE
+java -jar $0 "$SOURCE_CODE" > $OUTPUT_FILE
+
+exit 0
+
