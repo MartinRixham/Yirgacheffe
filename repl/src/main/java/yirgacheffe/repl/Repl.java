70faeeabbbdefb58a352ade1ab8df71d36b731c5
Martin Rixham <martin.rixham@gmail.com>
2018-06-11 21:32:59 +0100

    Repl echos string.

--- repl/src/main/java/yirgacheffe/repl/Repl.java
+++ repl/src/main/java/yirgacheffe/repl/Repl.java
@@ -1,13 +1,30 @@
 package yirgacheffe.repl;
 
+import java.util.Scanner;
+
 public final class Repl
 {
+	private Scanner scanner = new Scanner(System.in);
+
 	public static void main(String[] args)
 	{
-		System.out.print("yirgacheffe> ");
+		new Repl().read();
 	}
 
 	private Repl()
 	{
+	}
+
+	private void read()
+	{
+		do
+		{
+			System.out.print("yirgacheffe> ");
+
+			String line = this.scanner.nextLine();
+
+			System.out.println(line);
+		}
+		while (this.scanner.hasNextLine());
 	}
 }
--- repl/src/test/java/yirgacheffe/repl/ReplTest.java
+++ repl/src/test/java/yirgacheffe/repl/ReplTest.java
@@ -2,7 +2,9 @@ package yirgacheffe.repl;
 
 import org.junit.Test;
 
+import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
 import java.io.PrintStream;
 
 import static org.junit.Assert.assertEquals;
@@ -10,18 +12,67 @@ import static org.junit.Assert.assertEquals;
 public class ReplTest
 {
 	@Test
-	public void TestPrompt()
+	public void testPrompt()
 	{
 		PrintStream originalOut = System.out;
+		InputStream originalIn = System.in;
 		ByteArrayOutputStream spyOut = new ByteArrayOutputStream();
 		PrintStream out = new PrintStream(spyOut);
 
 		System.setOut(out);
 
+		InputStream in = new ByteArrayInputStream("\n".getBytes());
+		System.setIn(in);
+
 		Repl.main(null);
 
-		assertEquals("yirgacheffe> ", spyOut.toString());
+		assertEquals("yirgacheffe> \n", spyOut.toString());
 
 		System.setOut(originalOut);
+		System.setIn(originalIn);
+	}
+
+	@Test
+	public void testPrintString()
+	{
+		PrintStream originalOut = System.out;
+		InputStream originalIn = System.in;
+		ByteArrayOutputStream spyOut = new ByteArrayOutputStream();
+		PrintStream out = new PrintStream(spyOut);
+
+		System.setOut(out);
+
+		InputStream in = new ByteArrayInputStream("\"thingy\"".getBytes());
+		System.setIn(in);
+
+		Repl.main(null);
+
+		assertEquals("yirgacheffe> \"thingy\"\n", spyOut.toString());
+
+		System.setOut(originalOut);
+		System.setIn(originalIn);
+	}
+
+	@Test
+	public void testPrintingTwoStrings()
+	{
+		PrintStream originalOut = System.out;
+		InputStream originalIn = System.in;
+		ByteArrayOutputStream spyOut = new ByteArrayOutputStream();
+		PrintStream out = new PrintStream(spyOut);
+
+		System.setOut(out);
+
+		InputStream in = new ByteArrayInputStream("\"thingy\"\n\"sumpt\"".getBytes());
+		System.setIn(in);
+
+		Repl.main(null);
+
+		assertEquals(
+			"yirgacheffe> \"thingy\"\nyirgacheffe> \"sumpt\"\n",
+			spyOut.toString());
+
+		System.setOut(originalOut);
+		System.setIn(originalIn);
 	}
 }
