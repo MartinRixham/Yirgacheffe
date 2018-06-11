855d272fcbdfb09e5769ad61ef6abcd375fbf23d
Martin Rixham <martin.rixham@gmail.com>
2018-06-11 22:48:43 +0100

    Executable repl flag.

--- executable/script/run.sh
+++ executable/script/run.sh
@@ -2,8 +2,10 @@
 
 if [ $1 = --run ];then
     java -cp .:$0 $2
+elif [ $1 = --repl ];then
+    java -cp $0 yirgacheffe.repl.Repl
 else
-    java -cp $0 yirgacheffe.compiler.Yirgacheffe $1
+    java -cp $0 yirgacheffe.compiler.Yirgacheffe $*
 fi
 
 exit 0
