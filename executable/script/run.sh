f4202de21428edf58a3f1bdde97839f50bc1e69a
Martin Rixham <martin.rixham@gmail.com>
2019-04-28 18:55:23 +0100

    Fix run script.

--- executable/script/run.sh
+++ executable/script/run.sh
@@ -1,9 +1,9 @@
 #! /usr/bin/env bash
 
-if [ $1 = --run ];then
+if [ "$1" == --run ];then
     shift 1
     java -cp .:$0 $*
-elif [ $1 = --repl ];then
+elif [ "$1" == --repl ];then
     java -cp $0 yirgacheffe.repl.Repl
 else
     java -cp $0 yirgacheffe.compiler.Yirgacheffe $*
